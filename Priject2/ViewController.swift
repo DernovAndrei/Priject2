fd16f4262b96f3d6f73214ebc629cab443254e65
Andrei <dernovaw@gmail.com>
2022-01-05 00:19:32 -0600

    for fun

--- Priject2/ViewController.swift
+++ Priject2/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sliderFirts: UISlider!
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func useSlider(_ sender: Any) {
+        textLabel.text = String(sliderFirts.value)
+    }
+    
 }
 
